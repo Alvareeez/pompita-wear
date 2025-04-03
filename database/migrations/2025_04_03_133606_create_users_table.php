18a637b7fc60aadaafae97d36c972e1298e0eb85
alvareeezzz <daviddalvarez20@gmail.com>
2025-04-03 16:53:09 +0200

    MIGRATIONS - V2

--- database/migrations/2025_04_03_133606_create_users_table.php
+++ database/migrations/2025_04_03_133606_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('foto_perfil')->nullable();
             // RELACIONES ROLES
             $table->integer('id_rol')->nullable();
             $table->foreign('id_rol')->references('id_rol')->on('roles');
