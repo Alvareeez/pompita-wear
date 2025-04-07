e3a5c050661ed6b4a77887d23fddf8be80f61458
PC-ERIC <eric.alcazarc@gmail.com>
2025-04-07 17:02:34 +0200

    Comentarios web

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,14 @@ Route::get('/', function () {
     return view('cliente.index');
 })->middleware('auth');
 
+// RUTAS DE INICIO ---------------------------------------------------------------------------
 Route::view('/login', 'login.login')->name('login')->middleware('guest');
 Route::view('/registro', 'registro.registro')->middleware('guest');
 
 Route::post('/registro', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+
+
+// RUTAS DE SEGURIZADAS ---------------------------------------------------------------------------
+
