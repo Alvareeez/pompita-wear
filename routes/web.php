3c5773ab2e75a5c74da4161fa19e9305f7bc2111
alvareeezzz <daviddalvarez20@gmail.com>
2025-04-08 16:00:53 +0200

    PUSH TO MERGE

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/estilos/{id}/edit', [EstiloController::class, 'edit'])->name('admin.estilos.edit');
     Route::put('/estilos/{id}', [EstiloController::class, 'update'])->name('admin.estilos.update');
     Route::delete('/estilos/{id}', [EstiloController::class, 'destroy'])->name('admin.estilos.destroy');
-    Route::get('/outfit', function () {
-        return view('outfit');
-    })->middleware('auth');
 });
+Route::get('/outfit', function () {
+    return view('outfit');
+})->middleware('auth');
