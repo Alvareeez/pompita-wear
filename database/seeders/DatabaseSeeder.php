3a36cf3b7dab75607335b09b96978db87b2a65b3
Adrian Vazquez <100007005.joan23@fje.edu>
2025-04-08 16:05:05 +0200

    CRUD Etiquetas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Etiqueta;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,7 @@ class DatabaseSeeder extends Seeder
             EstilosSeeder::class,
             ColoresSeeder::class,
             PrendaSeeder::class,
+            EtiquetasSeeder::class,
         ]);
     }
     
