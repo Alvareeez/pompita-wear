d052af30d43f8a773ceed679c51bb37751849d35
alvareeezzz <daviddalvarez20@gmail.com>
2025-04-09 17:47:41 +0200

    ESTILOS

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -4,6 +4,11 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/perfil.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
+    </script>
 @endsection
 
 @section('content')
@@ -14,7 +19,8 @@
                 <h2>Editar Perfil</h2>
                 <form action="" enctype="multipart/form-data">
                     <!-- Foto de perfil -->
-                    <div class="profile-picture-container" onclick="document.getElementById('profile-picture-input').click()">
+                    <div class="profile-picture-container"
+                        onclick="document.getElementById('profile-picture-input').click()">
                         <img src="{{ asset($user->profile_picture ?? 'storage/profile_pictures/default.jpg') }}"
                             alt="Foto de perfil" class="profile-picture" id="profile-picture">
                         <div class="profile-picture-overlay">
