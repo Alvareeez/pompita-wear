9e70d7dd70dca94740da317d710ff97c7756a769
Adrian Vazquez <100007005.joan23@fje.edu>
2025-04-11 18:07:39 +0200

    Cambios CRUD Ropa

--- resources/views/Admin/ropa.blade.php
+++ resources/views/Admin/ropa.blade.php
@@ -90,10 +90,45 @@
             </table>
         </div>
 
+        <!-- Paginación -->
         <div class="pagination-container">
             {{ $prendas->links('pagination.custom') }}
         </div>
+
+        <!-- Formulario para descarga en PDF -->
+        <form action="{{ route('admin.ropa.pdf') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="prendas">Selecciona las prendas:</label>
+                <select id="prendas" name="prendas[]" multiple >
+                    @foreach ($prendas as $prenda)
+                        <option value="{{ $prenda->id_prenda }}">{{ $prenda->nombre }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button class="button" type="submit">
+                <span class="button__text">Descargar</span>
+                <span class="button__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg">
+                        <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                        <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                        <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+                    </svg>
+                </span>
+            </button>
+        </form>
     </main>
+
+    <div id="loading-spinner" class="dot-spinner" style="display: none;">
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+    </div>
 
     <script>
         function confirmDelete(id) {
@@ -115,14 +150,12 @@
 
         $(document).ready(function() {
             // Filtrar prendas cuando se seleccionen opciones
-            $('#filtro-nombre, #filtro-precio-min, #filtro-precio-max, #filtro-descripcion, #filtro-estilos, #filtro-etiquetas').on('input change', function() {
+            $('#filtro-nombre, #filtro-precio-min, #filtro-precio-max, #filtro-descripcion').on('input change', function() {
                 let nombre = $('#filtro-nombre').val();
                 let precio_min = $('#filtro-precio-min').val();
                 let precio_max = $('#filtro-precio-max').val();
                 let descripcion = $('#filtro-descripcion').val();
-                let estilos = $('#filtro-estilos').val();
-                let etiquetas = $('#filtro-etiquetas').val();
-    
+        
                 $.ajax({
                     url: '{{ route('admin.ropa.index') }}',
                     method: 'GET',
@@ -131,8 +164,6 @@
                         precio_min: precio_min,
                         precio_max: precio_max,
                         descripcion: descripcion,
-                        estilos: estilos,
-                        etiquetas: etiquetas,
                     },
                     beforeSend: function() {
                         $('#loading-spinner').show(); // Mostrar el spinner de carga
