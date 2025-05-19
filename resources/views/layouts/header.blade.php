1b4d4fceda6c822a284b6fdfaba421596fc4373c
Adrian Vazquez <100007005.joan23@fje.edu>
2025-05-19 15:15:12 +0200

    Cambio boton panel admin

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -47,15 +47,17 @@
                         @auth
                             @if (optional(auth()->user()->rol)->nombre === 'admin')
                                 <div class="input me-2">
-                                    <button class="admin-button">
-                                        <a href="{{ route('admin.usuarios.index') }}">
+                                    <a href="{{ route('admin.usuarios.index') }}" style="text-decoration:none;">
+                                        <button class="admin-button" type="button"
+                                            style="width:100%;display:flex;align-items:center;gap:8px;">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
                                                 <path fill-rule="evenodd" fill="#ffffff"
-                                                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
+                                                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"
+                                                    />
                                             </svg>
                                             Panel Admin
-                                        </a>
-                                    </button>
+                                        </button>
+                                    </a>
                                 </div>
                             @endif
 
